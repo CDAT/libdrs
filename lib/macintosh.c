92c41555b3af3d6b465a73b60a3b28d21799fd27
Jeffrey Painter <painter1@caradoc.llnl.gov.llnl.gov>
2011-06-03 11:36:16 -0700

    For 64-bit Macintoshes, use a local dictionary file.  When it is time to close it, first copy it to the output file which the user had requested.  This greatly improves performance if the user requested an output dictionary file in some remote networked location, since Macintosh OS X does not seem to cache such files.  This commit is of a somewhat unfinished product, but it works.

---
+++ lib/macintosh.c
@@ -0,0 +1,117 @@
+/* Macintosh-only functions in C .  Checked only for 64-bit Macintosh OS X */
+/* At the moment, this file only contains memory mapping code, which isn't used by default. */
+
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+static void * Data;
+static size_t DataLen;
+
+/* The following is adapted from
+ http://developer.apple.com/library/mac/#documentation/Performance/Conceptual/FileSystem/Articles/MappingFiles.html */
+
+/* usage example: 
+  Note that inPathName is input-only, a string.  And dataPtr, dataLength are needed only so that
+  munmap can unmap the right part of memory. */
+/*
+void ProcessFile( char * inPathName )
+{
+    size_t dataLength;
+    void * dataPtr;
+
+    if( MapFile( inPathName, &dataPtr, &dataLength ) == 0 )
+    {
+        //
+        // process the data and unmap the file
+        //
+        // . . .
+
+        munmap( dataPtr, dataLength );
+    }
+}
+*/
+
+ /* These little functions will let Fortran interface to mmap (through MapFile)
+ and munmap.   At the moment, assume that only _one_ file will be mapped.
+Later, I will have to relax this by building arrays in place of Data and DataLen.
+Even so, in that case I will assume that no file is mapped twice, and all will
+be unmapped together. */
+int mapf_( char * inPathName, int *PathNameLen ) {
+  char PathName[1024];    /* <<<< is there an existing parameter I can use for max path length? <<< */
+  strncpy(PathName,inPathName,*PathNameLen);
+  PathName[*PathNameLen]='\0';
+  printf( "jfp mapping %s length %i\n", inPathName,*PathNameLen );
+  return MapFile( PathName, &Data, &DataLen );
+}
+int unmapf_() {
+  munmap( Data, DataLen );
+  return 0;
+}
+ 
+// MapFile
+// Return the contents of the specified file as a read-only pointer.
+//
+// Enter:inPathName is a UNIX-style "/"-delimited pathname
+//
+// Exit:    outDataPtra     pointer to the mapped memory region
+//          outDataLength   size of the mapped memory region
+//          return value    an errno value on error (see sys/errno.h)
+//                          or zero for success
+//
+int MapFile( char * inPathName, void ** outDataPtr, size_t * outDataLength )
+{
+    int outError;
+    int fileDescriptor;
+    struct stat statInfo;
+ 
+    // Return safe values on error.
+    outError = 0;
+    *outDataPtr = NULL;
+    *outDataLength = 0;
+ 
+    // Open the file.
+    fileDescriptor = open( inPathName, O_RDONLY, 0 );
+    if( fileDescriptor < 0 )
+    {
+       outError = errno;
+    }
+    else
+    {
+        // We now know the file exists. Retrieve the file size.
+        if( fstat( fileDescriptor, &statInfo ) != 0 )
+        {
+            outError = errno;
+        }
+        else
+        {
+            // Map the file into a read-only memory region.
+	    // jfp added write permission
+            *outDataPtr = mmap(NULL,
+                                statInfo.st_size,
+                                PROT_READ | PROT_WRITE,
+                                0,
+                                fileDescriptor,
+                                0);
+            if( *outDataPtr == MAP_FAILED )
+            {
+                outError = errno;
+            }
+            else
+            {
+                // On success, return the size of the mapped file.
+                *outDataLength = statInfo.st_size;
+            }
+        }
+ 
+        // Now close the file. The kernel doesn’t use our file descriptor.
+	printf( "jfp finishing mmap with a close\n" );
+        close( fileDescriptor );
+    }
+ 
+    return outError;
+}
